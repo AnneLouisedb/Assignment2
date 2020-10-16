4a5252f3aa805e98f7809feacb5aaac6578bc154
AnneLouisedb <71494611+AnneLouisedb@users.noreply.github.com>
2020-10-16 20:29:11 +0200

    Creating a list of pipelines/models

--- src/modelling.py
+++ src/modelling.py
@@ -12,9 +12,11 @@ from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import ElasticNet
-from src.extra_functions import plot_model
+from sklearn.model_selection import GridSearchCV, cross_val_score, cross_val_predict
+from sklearn.linear_model import ElasticNet, SGDRegressor
+from copy import deepcopy
+from src.evaluation import evaluate_model
+from src.extra_functions import plot_model, plot_learning_curves
 
 data_dir = Path("data/")
 img_dir = Path("../img")
@@ -25,7 +27,6 @@ columns_to_use = [
     "Garage Area",
     "Bedroom AbvGr",
 ]
-
 
 all_data = pd.read_csv(data_dir / "housing-data.csv", index_col="Order")
 target_column = "SalePrice"
@@ -41,12 +42,23 @@ imputer = SimpleImputer(
     missing_values=np.nan, strategy="constant", fill_value=0
 )
 scaler = StandardScaler()
-linear_model = LinearRegression()
 
+#models
+linear_model = LinearRegression()
+ridge = Ridge()
+lasso = Lasso()
+elastic = ElasticNet()
+
+preprocess = Pipeline(steps = [("imputer", imputer), ("scale", scaler)])
+
+#models
 chosen_model = Pipeline(steps = [("imputer", imputer), ("scale", scaler), ("model", linear_model)])
 ridge_model = Pipeline(steps = [("imputer", imputer), ("scale", scaler), ("ridge.model", Ridge())])
 lasso_model = Pipeline(steps =[("imputer", imputer),("scale", scaler),  ("ridge.model", Lasso())])
 elastic_model = Pipeline(steps = [("imputer", imputer), ("scale", scaler), ("ridge.model", ElasticNet())])
+
+#list of models
+pipelines = [chosen_model, ridge_model, lasso_model, elastic_model]
 
 #Fitting regressors to the training set
 chosen_model.fit(X_train, y_train)
@@ -60,56 +72,51 @@ y_pred_ridge = ridge_model.predict(X_test)
 y_pred_lasso = lasso_model.predict(X_test)
 y_pred_elastic = elastic_model.predict(X_test)
 
+#accuracy testing
+
+evaluate_model(estimator= , X_test, y_test)
+
+#MAE
 print(f"mean_absolute_error linear: {mean_absolute_error(y_test, y_pred_linear)}")
 print(f"mean_absolute_error ridge:  {mean_absolute_error(y_test, y_pred_ridge)}")
 print(f"mean_absolute_error lasso:  {mean_absolute_error(y_test, y_pred_lasso)}")
-print(f"mean_absolute_error net elastic:  {mean_absolute_error(y_test, y_pred_elastic)}")
+print(f"mean_absolute_error net elastic: {mean_absolute_error(y_test, y_pred_elastic)}")
+
+
+
+
+
+
+
+
+
 
 #Learning curves
-def plot_learning_curves(model, X, y):
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.2, random_state=10
-    )
-    train_errors, val_errors = [], []
-    for m in range(1, len(X_train)):
-        model.fit(X_train[:m], y_train[:m])
-        y_train_predict = model.predict(X_train[:m])
-        y_val_predict = model.predict(X_val)
-        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
-        val_errors.append(mean_squared_error(y_val, y_val_predict))
-
-    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
-    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
-    plt.legend(loc="upper right", fontsize=14)
-    plt.xlabel("Training set size", fontsize=14)
-    plt.ylabel("RMSE", fontsize=14)
-
-#Plot learning curves
-plot_learning_curves(chosen_model, X_train, y_train)
-
-plot_learning_curves(ridge_model, X_train, y_train)
-
-plot_learning_curves(lasso_model, X_train, y_train)
-
-plot_learning_curves(elastic_model, X_train, y_train)
-
-
 
 #Create list of parameters for Ridge Regression
 #normalize = [ True, False]
-# solver = ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga']
-#parameters = dict(ridge__alpha = [1,10],ridge__normalize = normalize, ridge__solver = solver)
+#solver = ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga']
+#parameters = dict(ridge__alpha = [200, 230, 250,265, 270, 275, 290, 300, 500] ,ridge__normalize = normalize, ridge__solver = solver)
 #search_ridge = GridSearchCV(ridge_model, param_grid = parameters, cv = 5, scoring = 'accuracy' )
 
-#best estimator
-#clf = search_ridge.best_params_
-#clf.fit(X_train, y_train)
-#print(f'best sore and parameter combination: {search_ridge.best_score_} and {search_ridge.best_params_}')
+
+#ridge_params = {'alpha':[1e-15, 1e-10, 1e-8, 1e-4, 1e-2, 0.02, 0.024, 0.025, 0.026, 0.03, 1, 5, 10, 20, 200, 230, 250, 265, 270, 275, 290, 300, 500 ]}
+#lasso_params = {'alpha':[0.02, 0.024, 0.025, 0.026, 0.03]}
+
+
+#searchh = GridSearchCV(Ridge() , param_grid = ridge_params)
+#searchh.fit(X_train, y_train).best_estimator_
 
 
 
 #Hyper-parameter Tuning
 
+
+
 #Early Stopping
 
 #Interpreting Learning Curves
+
+
+
+
